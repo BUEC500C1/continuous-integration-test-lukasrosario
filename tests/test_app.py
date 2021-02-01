ec65db3fd21f5f95c96e01060cc4a8c26e0b10cf
Lukas <36800180+lukasrosario@users.noreply.github.com>
2021-02-01 12:30:49 -0500

    Additional tests and changes to dependencies (#2)
    * index + secrets test
    
    * actions workflow
    
    * tests check
    
    * util function tests
    
    * workflow change
    
    * api implementation
    
    * change index test
    
    * all initial tests
    
    * additional tests
    
    * dependencies fix
    
    * pull dev dependencies from requirements

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,17 +17,35 @@ def ask_question(client, question):
 
 
 def parseint(string):
-    m = re.findall(r"\d+(?:\.\d+)?", string)
+    m = re.findall(r"-?\d+(?:\.\d+)?", string)
     return m[0]
 
 
 distances = [
     ("12 feet to meters", 3.6576),
-    ("3 meters to feet", 9.84252),
+    ("convert 3 meters to feet", 9.84252),
     ("5000 meters to miles", 3.106856),
     ("what is 56 feet in inches", 672),
     ("what is 2 miles in meters", 3218.69),
-    ("?/&&  2 feet to inches \\  ??", 24),
+    ("?/&&  2feet to inches \\  ??", 24),
+]
+
+weights = [
+    ("25 oz to grams", 708.738),
+    ("convert 45 lbs to kg", 20.4117),
+    ("10 grams to lb", 0.0220462),
+    ("5329 oz to kilograms", 151.0746),
+    ("what is 100 kilograms in oz", 3527.4),
+    ("/&\\  29kg in g \\  \\??", 29000),
+]
+
+temperatures = [
+    ("25 c to f", 77),
+    ("convert 54 kelvin to c", -219.15),
+    ("1000 celsius to fahrenheit", 1832),
+    ("-64 fahrenheit to kelvin", 219.817),
+    ("what is 32 f in c", 0),
+    ("  \\//  290f in c \\  \\?&&  ", 143.333),
 ]
 
 
@@ -35,5 +53,36 @@ distances = [
 def test_ask_distances(client, question, expected_answer):
     rv = ask_question(client, question).data.decode()
     value = float(parseint(rv))
-    print(value)
-    assert 0.99 * expected_answer <= value <= 1.01 * expected_answer
+    assert 0.995 * abs(expected_answer) <= abs(value) <= 1.005 * abs(
+        expected_answer
+    ) and (
+        all(item > 0 for item in [value, expected_answer])
+        or all(item < 0 for item in [value, expected_answer])
+        or all(item == 0 for item in [value, expected_answer])
+    )
+
+
+@pytest.mark.parametrize("question,expected_answer", weights)
+def test_ask_weights(client, question, expected_answer):
+    rv = ask_question(client, question).data.decode()
+    value = float(parseint(rv))
+    assert 0.995 * abs(expected_answer) <= abs(value) <= 1.005 * abs(
+        expected_answer
+    ) and (
+        all(item > 0 for item in [value, expected_answer])
+        or all(item < 0 for item in [value, expected_answer])
+        or all(item == 0 for item in [value, expected_answer])
+    )
+
+
+@pytest.mark.parametrize("question,expected_answer", temperatures)
+def test_ask_temperatures(client, question, expected_answer):
+    rv = ask_question(client, question).data.decode()
+    value = float(parseint(rv))
+    assert 0.995 * abs(expected_answer) <= abs(value) <= 1.005 * abs(
+        expected_answer
+    ) and (
+        all(item > 0 for item in [value, expected_answer])
+        or all(item < 0 for item in [value, expected_answer])
+        or all(item == 0 for item in [value, expected_answer])
+    )
